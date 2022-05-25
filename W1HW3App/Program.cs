6bc2d47c2ec53cf273b887bb4d4dafa63689a124
RyenZhu <100649741+RyenZhu@users.noreply.github.com>
2022-05-25 22:36:18 +0800

    Add files via upload

---
+++ W1HW3App/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W1HW3App
+{
+    internal class LeapYear
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int year = int.Parse(Console.ReadLine());
+                if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+                {
+                    Console.WriteLine("{0} year is a leap", year);
+
+                }
+                else
+                    Console.WriteLine("{0} year is not a leap", year);
+            }
+
+
+
+        }
+    }
+}
