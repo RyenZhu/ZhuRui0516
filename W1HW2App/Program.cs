4c5373b5c5a94dfc68c501f6469a78fc816f3936
RyenZhu <100649741+RyenZhu@users.noreply.github.com>
2022-05-25 22:40:44 +0800

    Add files via upload

---
+++ W1HW2App/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W1HW2App
+{
+    class DayPrinter
+    {
+        static void Main(string[] args)
+        {
+            String[] day = new String[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(day[i]);
+            }
+
+        }
+
+    }
+}
